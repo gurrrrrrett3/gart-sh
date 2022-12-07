bdf6727881d8a453ca244ba199fb64d9709ed339
Garrett <gart@mangostudios.uk>
2022-12-07 01:15:19 -0500

    styling

--- modules/imageGen/mathChallenge.ts
+++ modules/imageGen/mathChallenge.ts
@@ -1,25 +1,30 @@
-import { createCanvas } from "@napi-rs/canvas";
+import { createCanvas, GlobalFonts } from "@napi-rs/canvas";
+import path from "path";
+
+GlobalFonts.registerFromPath(path.resolve("./asssets/RobotoMono-Regular.ttf"), "Roboto Mono");
 
 export default class MathChallenge {
 
     public static async mathChallenge() {
-        const canvas = createCanvas(1364, 480);
+        const canvas = createCanvas(600, 300);
         const ctx = canvas.getContext("2d");
 
         ctx.fillStyle = "#000000";
-        ctx.fillRect(0, 0, 1364, 480);
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
         ctx.fillStyle = "#ffffff";
-        ctx.font = "bold 48px sans-serif";
+        // monospace font
+        ctx.font = "48px Roboto Mono";
     
         const num1 = Math.floor(Math.random() * 20);
         const num2 = Math.floor(Math.random() * 20);
         const operator = Math.floor(Math.random() * 4);
 
         const operators = ["+", "-", "*", "/"];
-
-        ctx.fillText(`${num1} ${operators[operator]} ${num2}`, 10, 50);
-        ctx.fillText(`= ?`, 10, 100);
+        ctx.fillText("https://gart.sh", 10, 50);
+        ctx.fillText("$ math", 10, 100);
+        ctx.fillText(`${num1} ${operators[operator]} ${num2}`, 10, 200);
+        ctx.fillText(`= ?`, 10, 250);
 
         return canvas.toBuffer("image/webp");
     }
