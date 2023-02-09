1c5ccae6890b008faef51bbe084c23037e580db7
Garrett Keneau <gart@mangostudios.uk>
2023-02-09 10:07:56 -0500

    Update about.ts

--- sandbox/bin/about.ts
+++ sandbox/bin/about.ts
@@ -10,9 +10,8 @@ const about = {
     return [
         "<h1 class=\"green\">Hey! o/</h1>",
         "<p>I'm Gart, a 20 year old backend application developer. ",
-        "Most of my projects are in <span style=\"color: #2f74c0;\">TypeScript</span>, <span style=\"color: #dea584\">Rust</span>, and <span style=\"color: #f0931c;\">Java</span>.<br>",
+        "Most of my projects are in <span style=\"color: #2f74c0;\">TypeScript</span>.<br>",
         "Currently working on <a href=\"https://gart.sh\">gart.sh</a>, my personal website and sandbox.</p>",
-        "I'm currently a developer at <a href=\"https://craftyourtown.com\" style=\"color: #0d85d8\">CraftYourTown</a>. ",
         "Check out my <a href=\"https://github.com/gurrrrrrett3\">GitHub</a>!",
     ].join("")
   },
